08a1f686fc7949e2244115fd2ad040733685553f
Omri Zeevi <omriz@valu.co.il>
2021-10-06 17:02:25 +0300

    Added resource controller for model Trainee (only accessed if trainer id is valid)

---
+++ app/helpers/trainee_helper.rb
@@ -0,0 +1,2 @@
+module TraineeHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
+  resources :trainers do 
+    resources :trainees do
+      resources :workout_trainees
+    end
+    resources :workouts do
+      resources :workout_trainees
+    end
+  end
+  # The routes below are for my own practice
   get 'example/plain_text'
   get 'example/json_res'
   get 'example/xml_res'
   get 'example/view_res'
   get 'example/all_res'
-  resources :trainers
   get 'home/index';
   get 'home/json',  to: "home#json_res";
   get 'HelloWorld', to: "home#hello_world"; # This is case sensitive
