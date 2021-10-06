fc23c25006e31a7d10b9e0e83fa855556333fd6f
Omri Zeevi <omriz@valu.co.il>
2021-10-06 10:54:01 +0300

    Added bland action to contooller

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get 'home/index';
   get 'home/json',  to: "home#json_res";
   get 'HelloWorld', to: "home#hello_world"; # This is case sensitive
+  get 'JustPickME', controller: 'home', action: 'oi_pick_me'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
