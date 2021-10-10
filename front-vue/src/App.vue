61283fe6a52035cb6ef059fbd95e7e8106e0609f
Omri Zeevi <omriz@valu.co.il>
2021-10-10 14:49:28 +0300

    no actual change was made to the project

--- front-vue/src/App.vue
+++ front-vue/src/App.vue
@@ -1,25 +1,30 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
 
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  async created() {
+    try {
+      const response = await axios.get(`http://jsonplaceholder.typicode.com/posts`)
+      this.posts = response.data
+    } catch (e) {
+      this.errors.push(e)
+    }
   }
+
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
