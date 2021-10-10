98bda1dd998594cd64ff2d41aa2681a40f12f170
Omri Zeevi <omriz@valu.co.il>
2021-10-10 17:13:14 +0300

    fix issue with RoR serving html static file contained in View (It was trying to find old public static files that I don't need)

--- public/comps/trainer.js
+++ public/comps/trainer.js
@@ -1,23 +1,7 @@
 $(() => {
-    const redirectToRoot = () => { location.replace("/trainers");  }
+    const redirectToRoot = () => { location.replace("/trainers"); }
     const viewModel = new Vue({
         el: '#view',
-        data: {
-            TRAINER_ID_KEY: "trainer_id",
-            trainer: {},
-        },
-        methods: {
-            blandMethod() {
-                
-            }
-        },
-        created() {
-            if(!localStorage.getItem(this.TRAINER_ID_KEY)) {
-                redirectToRoot();
-                return;
-            }
-            console.log("Loading Trainer Model");
-            document.getElementById("model").textContent
-        },
+        
     });
 });
