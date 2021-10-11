f8657d6363d3c2a6a85573049a7b27521423c95d
Omri Zeevi <omriz@valu.co.il>
2021-10-11 18:23:19 +0300

    added the option add trainee

---
+++ front-vue/src/components/NewTraineeForm.vue
@@ -0,0 +1,51 @@
+<template>
+  <form @submit="submitNewTrainee" class="d-block container">
+    <h3>New Trainee Form</h3>
+    <div class="form-group mt-5 mb-4 w-50">
+      <label>Name:</label>
+      <input v-model="name" type="text" class="form-control">
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
+</template>
+
+<script>
+  import axios from 'axios';
+  export default {
+    name: 'NewTrainerForm',
+    data: function () {
+      return {
+        name: null,
+      }
+    },
+    props: {
+      trainerId: Number
+    },
+    methods: {
+      async submitNewTrainee() {
+        if(!this.name){
+          alert("Please set the name of the trainee ");
+          return;
+        }
+        try {
+          let res = await axios.post("/trainers/" + this.trainerId + "/trainees", { name : this.name });
+          console.log("response :: ",  res);
+          if(parseInt(res.status / 100) === 2) {
+            alert("trainer added successfully, press enter");
+            this.$emit('traineeAdded');
+          }
+        } catch (err) {
+          alert("An error occurred, please check console");
+          console.error("submitNewTrainer ::: ", err);
+        }
+      }
+    },
+    created() {
+      console.log(this.trainerId);
+    }    
+  }
+</script>
+
+<style scoped>
+
+</style>
--- front-vue/src/views/Trainees.vue
+++ front-vue/src/views/Trainees.vue
@@ -2,37 +2,49 @@
   <main class="d-block w-100">
     <h1 class="text-center">Trainees page</h1>
     <section class="w-75 m-auto">
-      <table class="table table-bordered">
-      <thead>
-        <tr>
-          <th> Id </th>
-          <th> Name </th>
-          <th> Last workout </th>
-          <th> Edit </th>
-          <th> Delete </th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="t in trainees" :key="t.id">
-          <td> {{ t.id }} </td>
-          <td> {{ t.name }} </td>
-          <td> {{ t.last_workout?.start_hour || "N/A" }} </td>
-          <td>
-            <button @click="editTrainee(t.id)" class="btn btn-secondary">Edit {{ t.name }} </button>
-          </td>
-          <td>
-            <button @click="removeTrainee(t.id)" class="btn btn-danger">Remove {{ t.name }} </button>
-          </td>
+      <table v-if="trainees && trainees.length > 0" class="table table-bordered">
+        <thead>
+          <tr>
+            <th> Id </th>
+            <th> Name </th>
+            <th> Last workout </th>
+            <th> Edit </th>
+            <th> Delete </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="t in trainees" :key="t.id">
+            <td> {{ t.id }} </td>
+            <td> {{ t.name }} </td>
+            <td> {{ t.last_workout?.start_hour || "N/A" }} </td>
+            <td>
+              <button @click="editTrainee(t.id)" class="btn btn-secondary">Edit {{ t.name }} </button>
+            </td>
+            <td>
+              <button @click="removeTrainee(t.id)" class="btn btn-danger">Remove {{ t.name }} </button>
+            </td>
         </tr>
       </tbody>
-    </table>
+      </table>
+      <div v-else>
+        <h4>You have no trainees, would you like to add?</h4>
+        <p>If so, fill the form below</p>
+      </div>
+    </section>
+    <hr/>
+    <section>
+      <TraineeForm :trainerId="trainerId" @traineeAdded="loadTrainees" />
     </section>
   </main>
 </template> 
 <script>
   import axios from 'axios';
+  import TraineeForm from '@/components/NewTraineeForm.vue'
   export default {
     name: 'Trainees',
+    components: {
+      TraineeForm
+    },
     data: function () {
       return {
         TRAINER_ID_KEY: "trainer_id",
@@ -61,7 +73,7 @@
       },
     },
     async created() { 
-      this.trainerId = this.$route.params["id"];
+      this.trainerId = parseInt(this.$route.params["id"]);
       await this.loadTrainees();
     },
   }
