c184221f24603a784e213bf2fc89c1c2ef03fbd8
Omri Zeevi <omriz@valu.co.il>
2021-10-12 13:35:14 +0300

    You can now add new Workouts to a trainer

--- front-vue/src/components/WorkoutForm.vue
+++ front-vue/src/components/WorkoutForm.vue
@@ -1,17 +1,24 @@
 <template>
-    <form @submit="submitNewTrainer" class="d-block container">
+    <form @submit="submit" class="d-block container">
     <h3>WorkoutForms</h3>
-    <!-- 
     <div class="form-group mt-5 mb-4 w-50">
       <label>Name:</label>
-      <input v-model="name" type="text" class="form-control">
+      <input v-model="workout.name" type="text" class="form-control">
+    </div>
+    <div class="form-group mt-5 mb-4 w-50">
+      <label>Duration in hours:</label>
+      <input v-model="workout.duration" type="number" min="0" class="form-control">
+    </div>
+    <div class="form-group mt-5 mb-4 w-50">
+      <label>Start Hour:</label>
+      <input v-model="workout.start_hour" type="datetime" class="form-control">
     </div>
     <button type="submit" class="btn btn-primary">Submit</button>
-    -->
   </form>
 </template>
 
 <script>
+    import axios from 'axios';
     export default {
         props: {
             trainerId: {
@@ -24,9 +31,39 @@
                 default: -1,
             },
         },
-        created() {
-            alert("Weclome my friend!");
-            alert("trainer id ::: " + this.trainerId + " || workout id ::: " + this.id);
+        data: function () {
+          return {
+            workout: { name: '', duration: 0, start_hour: new Date() , trainees: [] },
+            trainerTrainees: [],
+          }
+        },
+        computed: {
+            apiRoutePrefix: function() {
+                return "/trainers/" + this.trainerId;
+            },
+        },
+        methods: {
+            async submit() {
+                if(this.id === -1) {
+                    let res = await axios.post(this.apiRoutePrefix + "/workouts", this.workout);
+                    this.$emit('workoutAdded',res.data);
+                } else {
+                    alert("todo: PUT action")
+                }
+            }
+        },
+        async created() {
+            const apiRoutePrefix = "/trainers/" + this.trainerId;
+            // Load Trainer's Trainees for adding/removing
+            let trainerTraineesRes = await axios.get(apiRoutePrefix + "/trainees");
+            this.trainerTrainees = trainerTraineesRes.data;
+            if(this.id !== -1)  { // Load Relevent 
+                let workoutRes = await axios.get(apiRoutePrefix + "/workouts/" + this.id);
+                let workoutTraineesRes = await axios.get(apiRoutePrefix + "/workouts/" + this.id + "/trainees");
+                this.workout = workoutRes.data;
+                this.workout.start_hour = new Date(workoutRes.data.start_hour);
+                this.workout.trainees = workoutTraineesRes.data;
+            }
         },
     }
 </script>
--- front-vue/src/views/WorkoutNew.vue
+++ front-vue/src/views/WorkoutNew.vue
@@ -2,7 +2,7 @@
    <main>
     <h3 class="text-center"><a @click="goToWorkoutsPage">go back to trainer's workouts page</a></h3>
     <h1 class="text-center">New Workout</h1>
-    <WorkoutForm :trainerId="trainerId" />
+    <WorkoutForm @workoutAdded="goToNewelyAddedWorkout" :trainerId="trainerId" />
   </main>
 </template> 
 <script>
@@ -22,10 +22,14 @@
       goToWorkoutsPage() { 
         this.$router.push("/Trainer/" + this.trainerId + "/Workouts");
       },
+      goToNewelyAddedWorkout(newWorkout) {
+        let redirect = confirm("workout added successfully, redirect to Workout page ? no will go to trainer's workout page");
+        if(redirect) this.$router.push("/Trainer/" + this.trainerId + "/Workouts/" + newWorkout.id);
+        else this.$router.push("/Trainer/" + this.trainerId + "/Workouts");
+      }
     },
     created() {
       this.trainerId = parseInt(this.$route.params["trainerId"]);
-      console.log("trainerId ::: ",this.trainerId);
     },
   }
 </script>
