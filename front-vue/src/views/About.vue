fd6a683155346a6f84dac484dc6bf38d68e91255
Omri Zeevi <omriz@valu.co.il>
2021-10-13 17:31:41 +0300

    no actual change was made

--- front-vue/src/views/About.vue
+++ front-vue/src/views/About.vue
@@ -3,3 +3,17 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+<script>
+  export default {
+    name: 'About',
+    data: function () {
+      return {
+      }
+    },
+    methods: { 
+    },
+    created() {
+    },
+  }
+</script>
+<style></style>
