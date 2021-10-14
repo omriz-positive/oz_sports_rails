64423f89f3cd065aeb6f436a15920f7aa745d08f
Omri Zeevi <omriz@valu.co.il>
2021-10-14 18:12:23 +0300

    quick fix

--- app/serializers/trainee_serializer.rb
+++ app/serializers/trainee_serializer.rb
@@ -1,6 +1,6 @@
 class TraineeSerializer < ActiveModel::Serializer
   attributes :id, :name, :last_workout
   def last_workout
-    object.workouts.order_by(:start_hour).last
+    object.workouts.order(:start_hour).last
   end
 end
