3675b710469b3ebd1cd3aee82aae9e4468819f31
Taz Mania <OZcomingFRoo@gmail.com>
2021-10-15 16:02:23 +0300

    updated models

--- app/models/trainer.rb
+++ app/models/trainer.rb
@@ -1,6 +1,6 @@
 class Trainer < ApplicationRecord
-    has_many :trainees
-    has_many :workouts
+    has_many :trainees, dependent: :destroy 
+    has_many :workouts, dependent: :destroy 
     validates :name, presence: true
     validates_associated :trainees, :workouts
 end
